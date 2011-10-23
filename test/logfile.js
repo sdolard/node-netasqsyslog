43fefba1412ed84192793e48f66a4929123b142e
Sébasten Dolard <sdolard@gmail.com>
2011-10-23 22:12:07 +0200

    Optims and licence

--- test/logfile.js
+++ test/logfile.js
@@ -1,15 +1,42 @@
+/*
+Copyright © 2011 by Sebastien Dolard (sdolard@gmail.com)
+
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+*/
+
 var
 assert = require('assert'),
 util = require('util'),
 fs = require('fs'),
 logfile = require('../lib/logFile'),
 dataTest = [
-	'id=firewall time="2011-10-13 13:11:39" fw="fw_ihm" tz=+0200 ',
-	'startime="2011-10-13 13:09:38" pri=5 confid=01 slotlevel=2 ruleid=3 ',
-	'srcif="Ethernet1" srcifname="in" ipproto=udp dstif="Ethernet0" dstifname="out" ',
-	'proto=dns src=10.2.52.1 srcport=55765 srcportname=ephemeral_fw_udp dst=10.0.0.124 ',
-	'dstport=53 dstportname=domain_udp dstname=jupiter.netasq.com modsrc=10.2.52.1 ',
-	'modsrcport=55765 origdst=10.0.0.124 origdstport=53 sent=50 rcvd=89 duration=0.00\r\n'
+	'é1234567890AZERTYUIOPQSDFGHJKLMWXCVBN',
+	'1234567890AZERTYUIOPQSDFGHJKLMWXCVBN',
+	'1234567890AZERTYUIOPQSDFGHJKLMWXCVBN',
+	'1234567890AZERTYUIOPQSDFGHJKLMWXCVBN',
+	'1234567890AZERTYUIOPQSDFGHJKLMWXCVBN',
+	'1234567890AZERTYUIOPQSDFGHJKLMWXCVBN',
+	'1234567890AZERTYUIOPQSDFGHJKLMWXCVBN',
+	'1234567890AZERTYUIOPQSDFGHJKLMWXCVBN',
+	'1234567890AZERTYUIOPQSDFGHJKLMWXCVBN',
+	'1234567890AZERTYUIOPQSDFGHJKLMWXCVBN',
+	'1234567890AZERTYUIOPQSDFGHJKLMWXCVBN'
 ].join(''),
 endData = '',
 size = 0,
@@ -61,7 +88,7 @@ catch(exceptionBaz) {
 
 logFile = logfile.create({
 		directory: __dirname,
-		fileName: 'l_connection',
+		fileName: 'test.txt',
 		writeDelay: 1
 		
 });
@@ -78,34 +105,38 @@ logFile.on('written', function(fileName){
 			end = Date.now();
 			console.log('%dMB/s', size / 1024 / 1024 * 1000 / (end - start));
 		}
+		if (!endEvent) {
+			rs = fs.createReadStream(fileName, { 
+					encoding: 'utf8'
+			});
+			
+			rs.on('data', function (data) { 
+					if (elements === 1) {
+						endData += data; 
+					}
+			});
+			rs.on('end', function () { 
+					endEvent = true;
+			});
+			rs.on('error', function (exception) { 
+					console.log('ReadStream exception: %s(%s)', exception.message, exception.code);
+			});
+			rs.on('close', function () { 
+					// Clean up
+					fs.unlink(fileName, function (err) {
+							if (err) {
+								throw err;
+							}
+							closeEventUnlink = true;
+							
+					});
+					if (elements === 1) {
+						assert.equal(endData, dataTest);
+					}
+					logFile.write(dataTest); // this should throw an error (file donot exists more), but no.
+			});
+		}
 		
-		rs = fs.createReadStream(fileName, { 
-				encoding: 'utf8'
-		});
-		
-		rs.on('data', function (data) { 
-				if (elements === 1) {
-					endData += data; 
-				}
-		});
-		rs.on('end', function () { 
-				endEvent = true;
-		});
-		rs.on('error', function (exception) { 
-				console.log('exception: %s(%s)', exception.message, exception.code);
-		});
-		rs.on('close', function () { 
-				// Clean up
-				fs.unlink(fileName, function (err) {
-						if (err) {
-							throw err;
-						}
-						closeEventUnlink = true;
-				});
-				if (elements === 1) {
-					assert.equal(endData, dataTest);
-				}
-		});
 });
 
 if (elements > 1 ) {
